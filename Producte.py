1fc11b51cfd30c2df141af5e982a986f7a01d81b
Luca <lopritescu@institutmvm.cat>
2026-02-02 09:38:44 +0100

    Add files via upload

---
+++ Producte.py
@@ -0,0 +1,11 @@
+class Producte:
+    def __init__(self, nom, preu, stock):
+        self.nom = name
+        self.__preu = preu
+        self.stock = stock
+    def mostrar_preu(self, preu):
+        return self.__preu * 1.21
+    
+    def vendre_producte(Producte):
+        if self.stock == 0:
+            print("Error: No hi ha stock d'aquest producte:")
