6e98c6738633142fd18a192b9575aa9685d41bfd
Luca <lopritescu@institutmvm.cat>
2026-02-02 09:41:24 +0100

    Add files via upload

---
+++ Smartphone.py
@@ -0,0 +1,7 @@
+class Smartphone(Producte):
+    def __init__(self, nom, camera, bateria):
+        self.nom = nom
+        self.camera = camera
+        self.bateria = bateria
+    def import_extra(Smartphone):
+        return self.__preu + 5
